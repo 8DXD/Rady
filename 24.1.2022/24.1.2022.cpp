d4703405d3ac87e07a6cc18c62a65d12414e098e
Kateřina Dvořáková <kacule05@gmail.com>
2022-01-24 13:25:31 +0100

    Fibonacciho posloupnost

--- 24.1.2022/24.1.2022.cpp
+++ 24.1.2022/24.1.2022.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int minule = 0, predminule = 1 , fibonacci;
+	for (int i = 0; i < 30; i++) {
+		fibonacci = minule + predminule;
+		printf("Fibonacci (%2d) = %4d\n", i, fibonacci);
+		predminule = minule;
+		minule = fibonacci;
+	}
 }
